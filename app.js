d44e906aa30fe010935b6cb352128e91410059dc
xpwetx <kathrynjmiranda@gmail.com>
2026-02-24 16:32:52 -0800

    Initial project setup

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Connect to MongoDB
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB connected"))
+  .catch(err => console.log(err));
+
+app.get("/", (req, res) => {
+  res.send("API Running");
+});
+
+module.exports = app;
